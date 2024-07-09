73381fb65f8a9665cdf159b63146f5abee966e44
tstidwill <tara.stidwill@gmail.com>
2024-07-09 13:00:09 -0700

    Rename environement variables

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     charset: "utf8",
